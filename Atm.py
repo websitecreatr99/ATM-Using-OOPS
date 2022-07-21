f166b5d4ce52df490e8b79c3b20e093db38eda99
Kishan Pahadiya <85162734+websitecreatr99@users.noreply.github.com>
2022-07-21 18:34:32 +0700

    ATM USING OOPS

---
+++ Atm.py
@@ -0,0 +1,73 @@
+class Atm:
+    def __init__(self):
+        self.pin = ""
+        self.balance = 0
+
+        self.menu()
+
+    def menu(self):
+
+        user_input = input('''
+        Hello How would you like to proceed?
+        1. Enter 1 to create PIN
+        2. Enter 2 to deposit 
+        3. Enter 3 to withdraw
+        4. Enter 4 to check balance
+        5. Enter 5 to Exit
+        ''')
+
+        if user_input == 1:
+            self.create_pin()
+        elif user_input == 2:
+            self.deposit()
+        elif user_input == 3:
+            self.withdraw()
+        elif user_input == 4:
+            self.check_balance()
+        else user_input == 5:
+            print("BYE")
+
+    def create_pin(self):
+        self.pin = input("Enter your PIN")
+        print("PIN set Successfully")
+
+        self.menu()
+
+    def deposit(self):
+        temp = input("Enter your PIN")
+        if temp == self.pin:
+            amount = int(input("Enter the Amount"))
+            self.balance = self.balance + amount
+            print("Deposit Successful")
+        else:
+            print("Invalid PIN")
+
+        self.menu()
+
+    def withdraw(self):
+        temp = input("Enter your PIN")
+        if temp == self.pin:
+            amount = int(input("Enter the Amount"))
+            if self.balance < amount:
+                print("Insufficient Balance")
+            elif amount < self.balance:
+                self.balance = self.balance - amount
+                print("Operation Successful")
+        else:
+            print("Invalid PIN")
+
+        self.menu()
+
+    def check_balance(self):
+        temp = input("Enter your PIN")
+        if temp == self.pin:
+            print(self.balance)
+        else:
+            print("Invalid PIN")
+
+        self.menu()
+
+
+
+
+
