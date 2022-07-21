16281f6735556aafc7f91c74f42b7f557a3a36a9
Kishan Pahadiya <85162734+websitecreatr99@users.noreply.github.com>
2022-07-21 19:05:48 +0700

    Add files via upload

--- Atm.py
+++ Atm.py
@@ -1,11 +1,21 @@
 class Atm:
     def __init__(self):
-        self.pin = ""
-        self.balance = 0
+        self.__pin = ""
+        self.__balance = 0
 
-        self.menu()
+        self.__menu()
 
-    def menu(self):
+    def get_pin(self):
+        return self.__pin
+
+    def set_pin(self, new_pin):
+        if type(new_pin) == str:
+            self.__pin = new_pin
+            print("PIN Changed")
+        else:
+            print("Not Valid")
+
+    def __menu(self):
 
         user_input = input('''
         Hello How would you like to proceed?
@@ -24,48 +34,48 @@ class Atm:
             self.withdraw()
         elif user_input == 4:
             self.check_balance()
-        else user_input == 5:
+        elif user_input == 5:
             print("BYE")
 
     def create_pin(self):
-        self.pin = input("Enter your PIN")
+        self.__pin = input("Enter your PIN")
         print("PIN set Successfully")
 
-        self.menu()
+        self.__menu()
 
     def deposit(self):
         temp = input("Enter your PIN")
-        if temp == self.pin:
+        if temp == self.__pin:
             amount = int(input("Enter the Amount"))
-            self.balance = self.balance + amount
+            self.__balance = self.__balance + amount
             print("Deposit Successful")
         else:
             print("Invalid PIN")
 
-        self.menu()
+        self.__menu()
 
     def withdraw(self):
         temp = input("Enter your PIN")
-        if temp == self.pin:
+        if temp == self.__pin:
             amount = int(input("Enter the Amount"))
-            if self.balance < amount:
+            if self.__balance < amount:
                 print("Insufficient Balance")
-            elif amount < self.balance:
-                self.balance = self.balance - amount
+            elif amount < self.__balance:
+                self.__balance = self.__balance - amount
                 print("Operation Successful")
         else:
             print("Invalid PIN")
 
-        self.menu()
+        self.__menu()
 
     def check_balance(self):
         temp = input("Enter your PIN")
-        if temp == self.pin:
-            print(self.balance)
+        if temp == self.__pin:
+            print(self.__balance)
         else:
             print("Invalid PIN")
 
-        self.menu()
+        self.__menu()
 
 
 
